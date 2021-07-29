672519ca2d6c9215feeeccfbfb384361e0c60499
Mila-byte <tamila.r.1998@gmail.com>
2021-07-29 16:47:52 +0300

    first commit

---
+++ src/layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="base-layout">
+    <div class="main-header"></div>
+    <div class="main-container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout'
+}
+</script>
+
+<style></style>
